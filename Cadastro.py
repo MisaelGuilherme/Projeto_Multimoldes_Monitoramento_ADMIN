e3be4df216cdb29e8387c1b99d70792847c3ef5b
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2021-02-18 11:30:38 -0300

    Labels Inseridos no Frame Atribuicao

--- Cadastro.py
+++ Cadastro.py
@@ -20,13 +20,13 @@ class Front_End():
         self.frameDadosLogin = Frame(self.janelaCadastro, highlightbackground='grey', highlightthickness=2)
         self.frameDadosLogin.place(relx=0.100, rely=0.200, relwidth=0.800, relheight=0.250)
         
-        self.lbDados = Label(self.frameDadosLogin, text='Dados', font=('impact', 12))
+        self.lbDados = Label(self.frameDadosLogin, text='Dados', font=('impact', 14))
         self.lbDados.place(relx=0.010, rely=0.010)
         
-        self.lbNome = Label(self.frameDadosLogin, text='Nome', font=('impact',13))
-        self.lbCPF = Label(self.frameDadosLogin, text='CPF', font=('impact',13))
-        self.lbSenha = Label(self.frameDadosLogin, text='Senha', font=('impact',13))
-        self.lbConfirmaSenha = Label(self.frameDadosLogin, text='Confirme Senha', font=('impact',13))
+        self.lbNome = Label(self.frameDadosLogin, text='Nome', font=('impact',12))
+        self.lbCPF = Label(self.frameDadosLogin, text='CPF', font=('impact',12))
+        self.lbSenha = Label(self.frameDadosLogin, text='Senha', font=('impact',12))
+        self.lbConfirmaSenha = Label(self.frameDadosLogin, text='Confirme Senha', font=('impact',12))
         
         self.lbNome.place(relx=0.020, rely=0.300)
         self.lbCPF.place(relx=0.380, rely=0.300)
@@ -34,10 +34,10 @@ class Front_End():
         self.lbConfirmaSenha.place(relx=0.270, rely=0.600)
         
 
-        self.campoNome = Entry(self.frameDadosLogin, font=('arial',13))
-        self.campoCPF = Entry(self.frameDadosLogin, font=('arial',13))
-        self.campoSenha = Entry(self.frameDadosLogin, font=('arial',13))
-        self.campoConfirmaSenha = Entry(self.frameDadosLogin, font=('arial',13))
+        self.campoNome = Entry(self.frameDadosLogin, font=('arial',12))
+        self.campoCPF = Entry(self.frameDadosLogin, font=('arial',12))
+        self.campoSenha = Entry(self.frameDadosLogin, font=('arial',12))
+        self.campoConfirmaSenha = Entry(self.frameDadosLogin, font=('arial',12))
         
         self.campoNome.place(relx=0.080, rely=0.300, relwidth=0.280)
         self.campoCPF.place(relx=0.425, rely=0.300, relwidth=0.175)
@@ -47,35 +47,51 @@ class Front_End():
         self.frameAtribuicao = Frame(self.janelaCadastro, highlightbackground='grey', highlightthickness=2)
         self.frameAtribuicao.place(relx=0.100, rely=0.460, relwidth=0.800, relheight=0.250)
         
-        self.lbAtribuicao = Label(self.frameAtribuicao, text='Atribuição', font=('impact', 12))
+        self.lbAtribuicao = Label(self.frameAtribuicao, text='Atribuição', font=('impact', 14))
         self.lbAtribuicao.place(relx=0.010, rely=0.010)
         
+        self.lbSerramento = Label(self.frameAtribuicao, text='Serramento', font=('impact', 11))
+        self.lbAplainamento = Label(self.frameAtribuicao, text='Aplainamento', font=('impact', 11))
+        self.lbTorneamento = Label(self.frameAtribuicao, text='Torneamento', font=('impact', 11))
+        self.lbFresagem = Label(self.frameAtribuicao, text='Fresagem', font=('impact', 11))
+        self.lbFurar = Label(self.frameAtribuicao, text='Furar', font=('impact', 11))
+        self.lbBrochamento = Label(self.frameAtribuicao, text='Brochamento', font=('impact', 11))
+        self.lbEletroerosao = Label(self.frameAtribuicao, text='Eletroerosao', font=('impact', 11))
+        
+        self.lbSerramento.place(relx=0.020, rely=0.400)
+        self.lbAplainamento.place(relx=0.15, rely=0.400)
+        self.lbTorneamento.place(relx=0.28, rely=0.400)
+        self.lbFresagem.place(relx=0.41, rely=0.400)
+        self.lbFurar.place(relx=0.54, rely=0.400)
+        self.lbBrochamento.place(relx=0.67, rely=0.400)
+        self.lbEletroerosao.place(relx=0.80, rely=0.400)
+
         self.lbbox1 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox1['values'] = ('Select', '1', '2', '3')
+        self.lbbox1['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox1.current(0)
         
         self.lbbox2 = ttk.Combobox(self.frameAtribuicao, font=('arial', 13), state="readonly")
-        self.lbbox2['values'] = ('Select', '1', '2', '3')
+        self.lbbox2['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox2.current(0)
 
         self.lbbox3 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox3['values'] = ('Select', '1', '2', '3')
+        self.lbbox3['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox3.current(0)
     
         self.lbbox4 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox4['values'] = ('Select', '1', '2', '3')
+        self.lbbox4['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox4.current(0)
         
         self.lbbox5 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox5['values'] = ('Select', '1', '2', '3')
+        self.lbbox5['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox5.current(0)
         
         self.lbbox6 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox6['values'] = ('Select', '1', '2', '3')
+        self.lbbox6['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox6.current(0)
         
         self.lbbox7 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox7['values'] = ('Select', '1', '2', '3')
+        self.lbbox7['values'] = ('Select', '0', '1', '2', '3')
         self.lbbox7.current(0)        
                         
         self.lbbox1.place(relx=0.020, rely=0.600, relwidth=0.11)
