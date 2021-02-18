97eae08263bd1b20c5b54c7749fc76ed30a8c2ca
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2021-02-18 10:54:21 -0300

    Campos de Atribuição Criados

--- Cadastro.py
+++ Cadastro.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from platform import *
 
 class Front_End():
@@ -16,7 +17,7 @@ class Front_End():
         else:
             self.janelaCadastro.attributes('-zoomed', True)
         
-        self.frameDadosLogin = Frame(self.janelaCadastro, highlightbackground='black', highlightthickness=2)
+        self.frameDadosLogin = Frame(self.janelaCadastro, highlightbackground='grey', highlightthickness=2)
         self.frameDadosLogin.place(relx=0.100, rely=0.200, relwidth=0.800, relheight=0.250)
         
         self.lbDados = Label(self.frameDadosLogin, text='Dados', font=('impact', 12))
@@ -42,6 +43,48 @@ class Front_End():
         self.campoCPF.place(relx=0.425, rely=0.300, relwidth=0.175)
         self.campoSenha.place(relx=0.085, rely=0.600, relwidth=0.175)
         self.campoConfirmaSenha.place(relx=0.410, rely=0.600, relwidth=0.175)
+        
+        self.frameAtribuicao = Frame(self.janelaCadastro, highlightbackground='grey', highlightthickness=2)
+        self.frameAtribuicao.place(relx=0.100, rely=0.460, relwidth=0.800, relheight=0.250)
+        
+        self.lbAtribuicao = Label(self.frameAtribuicao, text='Atribuição', font=('impact', 12))
+        self.lbAtribuicao.place(relx=0.010, rely=0.010)
+        
+        self.lbbox1 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox1['values'] = ('Select', '1', '2', '3')
+        self.lbbox1.current(0)
+        
+        self.lbbox2 = ttk.Combobox(self.frameAtribuicao, font=('arial', 13), state="readonly")
+        self.lbbox2['values'] = ('Select', '1', '2', '3')
+        self.lbbox2.current(0)
+
+        self.lbbox3 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox3['values'] = ('Select', '1', '2', '3')
+        self.lbbox3.current(0)
+    
+        self.lbbox4 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox4['values'] = ('Select', '1', '2', '3')
+        self.lbbox4.current(0)
+        
+        self.lbbox5 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox5['values'] = ('Select', '1', '2', '3')
+        self.lbbox5.current(0)
+        
+        self.lbbox6 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox6['values'] = ('Select', '1', '2', '3')
+        self.lbbox6.current(0)
+        
+        self.lbbox7 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.lbbox7['values'] = ('Select', '1', '2', '3')
+        self.lbbox7.current(0)        
+                        
+        self.lbbox1.place(relx=0.020, rely=0.600, relwidth=0.11)
+        self.lbbox2.place(relx=0.15, rely=0.600, relwidth=0.11)
+        self.lbbox3.place(relx=0.28, rely=0.600, relwidth=0.11)
+        self.lbbox4.place(relx=0.41, rely=0.600, relwidth=0.11)
+        self.lbbox5.place(relx=0.54, rely=0.600, relwidth=0.11)
+        self.lbbox6.place(relx=0.67, rely=0.600, relwidth=0.11)
+        self.lbbox7.place(relx=0.80, rely=0.600, relwidth=0.11)
 
         self.janelaCadastro.mainloop()
 
