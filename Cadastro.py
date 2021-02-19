6a369c5613ba9bd4b7d89716b055cc75164f5f8f
Misael Guilherme <63935572+MisaelGuilherme@users.noreply.github.com>
2021-02-19 10:46:24 -0300

    Verificação Boxes Não Selecionados e Estruturação do Código

--- Cadastro.py
+++ Cadastro.py
@@ -14,9 +14,19 @@ class Back_End():
         c = self.campoSenha.get()
         d = self.campoConfirmaSenha.get()
         
-        #Verificando se algum campo não foi preenchido
+        #Atribuição dos valores dos boxes de seleção nas variáves
         
-        if a == '' or b == '' or c == '' or d == '':
+        A = self.box1.get()
+        B = self.box2.get()
+        C = self.box3.get()
+        D = self.box4.get()
+        E = self.box5.get()
+        F = self.box6.get()
+        G = self.box7.get()
+        
+        #Verificando se algum campo não foi preenchido
+        print(len(a))
+        if a == '' or b == '' or c == '' or d == '' or A == 'Select' or B == 'Select' or C == 'Select' or D == 'Select' or E == 'Select' or F == 'Select' or G == 'Select':
             
             if a == '':
                 
@@ -38,7 +48,48 @@ class Back_End():
                 self.campoConfirmaSenha['bg'] = 'pink'
                 self.lbConfirmaSenha['fg'] = 'red'
             
+            if A == 'Select':
+                self.lbSerramento['fg'] = 'red'
+            
+            if B == 'Select':
+                self.lbAplainamento['fg'] = 'red'
+            
+            if C == 'Select':
+                self.lbTorneamento['fg'] = 'red'
+            
+            if D == 'Select':
+                self.lbFresagem['fg'] = 'red'
+            
+            if F == 'Select':
+                self.lbFurar['fg'] = 'red'
+            
+            if G == 'Select':
+                self.lbBrochamento['fg'] = 'red'
+            
+            if E == 'Select':
+                self.lbEletroerosao['fg'] = 'red'
+            
             return messagebox.showerror('Alerta', 'Verifique os campos')
+        
+        if len(a) < 6:
+                
+                self.campoNome['bg'] = 'pink'
+                self.lbNome['fg'] = 'red'
+            
+        if len(b) < 11:
+            
+            self.campoCPF['bg'] = 'pink'
+            self.lbCPF['fg'] = 'red'
+        
+        if len(c) < 8:
+
+            self.campoSenha['bg'] = 'pink'
+            self.lbSenha['fg'] = 'red'
+    
+        if len(d) < 8:
+    
+            self.campoConfirmaSenha['bg'] = 'pink'
+            self.lbConfirmaSenha['fg'] = 'red'
         
         else: messagebox.showinfo('Alerta', 'Usuário cadastrado com sucesso!')
         
@@ -174,41 +225,41 @@ class Front_End(Back_End):
         
         #Boxes de seleção para o nível de habilidades do usuário em cada operação
 
-        self.lbbox1 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox1['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox1.current(0)
+        self.box1 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box1['values'] = ('Select', '0', '1', '2', '3')
+        self.box1.current(0)
         
-        self.lbbox2 = ttk.Combobox(self.frameAtribuicao, font=('arial', 13), state="readonly")
-        self.lbbox2['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox2.current(0)
+        self.box2 = ttk.Combobox(self.frameAtribuicao, font=('arial', 13), state="readonly")
+        self.box2['values'] = ('Select', '0', '1', '2', '3')
+        self.box2.current(0)
 
-        self.lbbox3 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox3['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox3.current(0)
+        self.box3 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box3['values'] = ('Select', '0', '1', '2', '3')
+        self.box3.current(0)
     
-        self.lbbox4 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox4['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox4.current(0)
+        self.box4 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box4['values'] = ('Select', '0', '1', '2', '3')
+        self.box4.current(0)
         
-        self.lbbox5 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox5['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox5.current(0)
+        self.box5 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box5['values'] = ('Select', '0', '1', '2', '3')
+        self.box5.current(0)
         
-        self.lbbox6 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox6['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox6.current(0)
+        self.box6 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box6['values'] = ('Select', '0', '1', '2', '3')
+        self.box6.current(0)
         
-        self.lbbox7 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
-        self.lbbox7['values'] = ('Select', '0', '1', '2', '3')
-        self.lbbox7.current(0)        
+        self.box7 = ttk.Combobox(self.frameAtribuicao, font=('arial',13), state="readonly")
+        self.box7['values'] = ('Select', '0', '1', '2', '3')
+        self.box7.current(0)        
                         
-        self.lbbox1.place(relx=0.020, rely=0.600, relwidth=0.11)
-        self.lbbox2.place(relx=0.15, rely=0.600, relwidth=0.11)
-        self.lbbox3.place(relx=0.28, rely=0.600, relwidth=0.11)
-        self.lbbox4.place(relx=0.41, rely=0.600, relwidth=0.11)
-        self.lbbox5.place(relx=0.54, rely=0.600, relwidth=0.11)
-        self.lbbox6.place(relx=0.67, rely=0.600, relwidth=0.11)
-        self.lbbox7.place(relx=0.80, rely=0.600, relwidth=0.11)
+        self.box1.place(relx=0.020, rely=0.600, relwidth=0.11)
+        self.box2.place(relx=0.15, rely=0.600, relwidth=0.11)
+        self.box3.place(relx=0.28, rely=0.600, relwidth=0.11)
+        self.box4.place(relx=0.41, rely=0.600, relwidth=0.11)
+        self.box5.place(relx=0.54, rely=0.600, relwidth=0.11)
+        self.box6.place(relx=0.67, rely=0.600, relwidth=0.11)
+        self.box7.place(relx=0.80, rely=0.600, relwidth=0.11)
         
         #Botão que confirmará os dados quando solicitado
         
